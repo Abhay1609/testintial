0a9ff5b09f031422602134bc577d5f60e9e4fb90
Raghav Awasthi <raghavawasthi240@gmail.com>
2023-02-03 15:34:09 +0530

    student no field added

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,9 +4,10 @@ import "./Form.css";
 const Form = () => {
 
     const initialvalues={
-        name:"",
-        roll:"",
-        contact:"",
+        full_name:"",
+        roll_no:"",
+        student_no:"",
+        mobile_number:"",
         email:"",
         branch:"",
         year:"",
@@ -32,36 +33,51 @@ const Form = () => {
         const regex_name=/^[a-zA-Z]{3,15}(\s[a-zA-Z]{2,10})?$/;
         const regex_contact= /^[6-9]([0-9]){9}$/;
         const regex_roll=/^[2][12][0][0][2][7][01]([0-9]){6}$/;
+        const regex_student=/^[2][12]([0-9]){5,6}$/;
         const regex_email=/^([a-z]){3,15}([0-9]){7,10}@akgec.ac.in$/;
         
         //VALIDATING NAME
-        if(regex_name.test(formvalues.name.trim())){
-          formvalues.name=formvalues.name.trim();
-          errors.name="";
+        if(regex_name.test(formvalues.full_name.trim())){
+          formvalues.full_name=formvalues.full_name.trim();
+          errors.full_name="";
         }
         else{
           setnoerror(false);
-          errors.name="error in name";
+          errors.full_name="error in name";
+          return errors;
+        }
+
+        //VALIDATING STUDENTNO
+        if(regex_student.test(formvalues.student_no.trim())){
+          errors.student_no="";
+          formvalues.student_no=formvalues.student_no.trim();
+        }
+        else{
+          setnoerror(false);
+          errors.student_no="Invalid Student no";
+          return errors;
         }
 
         //VALIDATING ROLLNO
-        if(regex_roll.test(formvalues.roll.trim())){
-          errors.roll="";
-          formvalues.roll=formvalues.roll.trim();
+        if(regex_roll.test(formvalues.roll_no.trim())){
+          errors.roll_no="";
+          formvalues.roll_no=formvalues.roll_no.trim();
         }
         else{
           setnoerror(false);
-          errors.roll="Invalid roll no"
+          errors.roll_no="Invalid roll no";
+          return errors;
         }
 
         //VALIDATING CONTACT
-        if(regex_contact.test(formvalues.contact.trim())){
-            errors.contact="";
-            formvalues.contact=formvalues.contact.trim();
+        if(regex_contact.test(formvalues.mobile_number.trim())){
+            errors.mobile_number="";
+            formvalues.mobile_number=formvalues.mobile_number.trim();
         }
         else{
             setnoerror(false)
-            errors.contact="**Invalid Mobile Number";
+            errors.mobile_number="**Invalid Mobile Number";
+            return errors;
         }
 
         //VALIDATING EMAIL
@@ -72,42 +88,51 @@ const Form = () => {
         else{
           setnoerror(false);
           errors.email="invalid email";
+          return errors;
         }
 
         //VALIDATING BRANCH
         if(formvalues.branch==""){
           setnoerror(false);
           errors.branch="Select branch";
+          return errors;
         }
         else{
           errors.branch="";
+          
         }
 
         //VALIDATING YEAR
         if(formvalues.year==""){
           setnoerror(false);
           errors.year="Select Year";
+          return errors;
         }
         else{
           errors.year="";
+          
         }
   
         //VALIDATING GENDER
         if(formvalues.gender==""){
           setnoerror(false);
           errors.gender="Select gender";
+          return errors;
         }
         else{
           errors.gender="";
+          
         }
 
         //VALIDATING RESIDENCE
         if(formvalues.residence==""){
           setnoerror(false);
           errors.residence="Select residence";
+          return errors;
         }
         else{
           errors.residence="";
+          
         }
 
         return errors;
@@ -160,18 +185,23 @@ const Form = () => {
     <div>
         <form onSubmit={validateform}>
             <div className='name-sec'>
-              <input type="text" placeholder="Name" name="name" value={formvalues.name} onChange={inputHandler}/>
-              <p>{formerror.name}</p>
+              <input type="text" placeholder="Name" name="full_name" value={formvalues.full_name} onChange={inputHandler}/>
+              <p>{formerror.full_name}</p>
+           </div>
+
+           <div className='student_no-sec'>
+              <input type="text" placeholder="Student No." name='student_no' value={formvalues.student_no} onChange={inputHandler}/>
+              <p>{formerror.student_no}</p>
            </div>
 
            <div className='roll-sec'>
-              <input type="text" placeholder="University Roll No." name='roll' value={formvalues.roll} onChange={inputHandler}/>
-              <p>{formerror.roll}</p>
+              <input type="text" placeholder="University Roll No." name='roll_no' value={formvalues.roll_no} onChange={inputHandler}/>
+              <p>{formerror.roll_no}</p>
            </div>
 
            <div className='contact-sec'>
-              <input type="text" placeholder="Contact No." name="contact" value={formvalues.contact} onChange={inputHandler}/>
-              <p>{formerror.contact}</p>
+              <input type="text" placeholder="Contact No." name="mobile_number" value={formvalues.mobile_number} onChange={inputHandler}/>
+              <p>{formerror.mobile_number}</p>
            </div>
 
            <div className='email-sec'>
